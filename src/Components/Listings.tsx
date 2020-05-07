340d6c9ab8001ad0041a96300d37b6509e87bb31
Dev Govindji <friendsofdev94@gmail.com>
2020-05-07 14:19:20 -0500

    stubs up api

--- src/Components/Listings.tsx
+++ src/Components/Listings.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, CardTitle, CardSubtitle, CardText, CardImg, CardBody } from 'reactstrap'
 
 const Listings = () => {
+
+    const [pets, setPets] = useState([])
+
+    useEffect(() => {
+        const url = 'https://api.rescuegroups.org/v5/public/animals/'
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/vnd.api+json");
+        myHeaders.append("Authorization", "M4heqARw");
+        // const headers = {
+        //     "Content-Type": "application/vnd.api+json",
+        //     "Authorization": "M4heqARw"
+        // }
+        // var raw = "{\n    \"data\": {\n        \"filters\": \n    	[\n    		{\n    			\"fieldName\": \"animals.breedPrimaryId\",\n    			\"operation\": \"equal\",\n    			\"criteria\": \"90\"\n    		},\n    		{\n    			\"fieldName\": \"animals.sizeGroup\",\n    			\"operation\": \"equal\",\n    			\"criteria\": [\"Small\",\"Medium\"]\n    		}\n    	],\n    	\"filterProcessing\": \"1 and 2\",\n        \"filterRadius\":\n        	{\n        		\"miles\": 100,\n        		\"lat\": 34.1031,\n        		\"lon\": -118.416\n        	}\n        \n    }\n}\n";
+
+        var requestOptions = {
+            // method: 'GET',
+            headers: myHeaders,
+            // body: raw,
+            // redirect: 'follow'
+        };
+
+        fetch(url, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                setPets(result.data)
+                // console.log(result.data.slice(0, 10))
+                console.log(pets)
+            }
+            )
+
+
+    })
 
 
 
@@ -9,28 +41,11 @@ const Listings = () => {
         <div>
             Listings
 
+            <p> {Object.keys(pets)} </p>
+            <p> {typeof (pets[0])} </p>
+            {/* <p> {pets.slice(0, 10)} </p> */}
 
-            <div className="Cards">
-                <Card className="card">
-                    <CardImg top width="100%" alt="Otis" />
-                    <CardBody>
-                        <CardTitle>Otis</CardTitle>
-                        <CardSubtitle>Border Collie</CardSubtitle>
-                        <CardText>Age: 1 years old, Black, Male, 20 lbs.</CardText>
-                        {/* <Link to="/apply">Apply</Link> */}
-                    </CardBody>
-                </Card>
-                <Card className="card">
-                    <CardImg top width="100%" alt="Dana" />
-                    <CardBody>
-                        <CardTitle>Dana</CardTitle>
-                        <CardSubtitle>Domestic Shorthair</CardSubtitle>
-                        <CardText>Age: 3 years old, Black, Female, 10 lbs.</CardText>
-                        {/* <Link to="/apply">Apply</Link> */}
-                    </CardBody>
-                </Card>
 
-            </div>
         </div>
     )
 
